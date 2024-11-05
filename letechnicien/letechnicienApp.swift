e09c3142febabe84c75753f93d8ea7a212856616
M3LMO <melm.mounir@gmail.com>
2024-11-05 11:23:34 +0100

    Initial Commit

---
+++ letechnicien/letechnicienApp.swift
@@ -0,0 +1,17 @@
+//
+//  letechnicienApp.swift
+//  letechnicien
+//
+//  Created by Melm on 05/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct letechnicienApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ letechnicienTests/letechnicienTests.swift
@@ -0,0 +1,17 @@
+//
+//  letechnicienTests.swift
+//  letechnicienTests
+//
+//  Created by Melm on 05/11/2024.
+//
+
+import Testing
+@testable import letechnicien
+
+struct letechnicienTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
